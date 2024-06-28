502307efe79b7393785c35bbc4dbe67ab7625a15
Albus-489 <smartoctopusz@gmail.com>
2024-06-28 16:39:13 +0300

    sidebar

--- src/Components/smith-nav/smith-nav.component.jsx
+++ src/Components/smith-nav/smith-nav.component.jsx
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './smith-nav.style.css';
 import logo from '../../assets/images/SmithStyleLogo340x90.png';
 import { Link } from 'react-router-dom';
 import { NavOptionsCollection } from '../../assets/collections/nav-options.collection';
+import { CiMenuFries } from 'react-icons/ci';
 
 export const SmithNavComponent = () => {
+  const [sideMenuVisible, setSideMenu] = useState(false);
+
+  const toggleSideMenu = () => {
+    setSideMenu(!sideMenuVisible);
+  };
+
+  const closeSideMenu = () => {
+    setSideMenu(false);
+  };
+
   return (
-    <div className="smith-nav flex justify-between my-7 h-16 md:mx-3 lg:mx-20">
+    <div className="smith-nav flex justify-between max-sm:px-6 px-14 py-6 my-7 h-16 md:mx-3 lg:mx-20">
       <div className="logo select-none cursor-pointer">
         <Link to="/">
-          <img width={220} src={logo} alt="" />
+          <img className='w-52' src={logo} alt="SmithStyleLogo" />
         </Link>
       </div>
-      <ul className="options flex gap-20 align-middle select-none text-blue-400 md:text-xs lg:text-xl">
-        {NavOptionsCollection.map((option) => {
-          return (
-            <li className="option oprion-home inline-block hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
-              <Link to={option.path}>{option.name}</Link>
-            </li>
-          );
-        })}
+      <ul className="options max-md:hidden flex gap-20 align-middle select-none text-md lg:text-xl ease-in transition-all">
+        {NavOptionsCollection.map((option, index) => (
+          <li
+            key={index}
+            className="option oprion-home inline-block hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
+            <Link to={option.path}>{option.name}</Link>
+          </li>
+        ))}
       </ul>
+      <div className="md:hidden flex justify-center">
+        <button onClick={toggleSideMenu} className="text-3xl">
+          <CiMenuFries />
+        </button>
+      </div>
+
+      <div
+        className={`fixed inset-0 bg-gray-800 bg-opacity-75 z-50 transform transition-transform duration-300 ${
+          sideMenuVisible ? 'translate-x-0' : 'translate-x-full'
+        } md:hidden`}>
+        <div className="relative h-full w-64 bg-stone-900 p-4">
+          <button
+            onClick={closeSideMenu}
+            className="absolute top-4 right-4 text-xl">
+            ✕
+          </button>
+          <ul className="mt-10 space-y-4">
+            {NavOptionsCollection.map((option, index) => (
+              <li
+                key={index}
+                className="option oprion-home hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
+                <Link to={option.path} onClick={closeSideMenu}>
+                  {option.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
-{
-  /* <li className="option oprion-home inline-block hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
-          <Link to="/discord-navbar">Home</Link>
-        </li>
-        <li className="option oprion-cv inline-block hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
-          CV
-        </li>
-        <li className="option oprion-about inline-block hover:bg-gradient-to-r from-pink-400 to-blue-300 hover:text-transparent hover:bg-clip-text">
-          About
-        </li> */
-}
