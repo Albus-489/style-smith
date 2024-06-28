cbbf4a9a2f2ede4f1441119dd2217f484bb0d463
Albus-489 <smartoctopusz@gmail.com>
2024-06-28 16:37:47 +0300

    data colections

--- src/assets/collections/nav-options.collection.js
+++ src/assets/collections/nav-options.collection.js
@@ -1,5 +1,5 @@
 export const NavOptionsCollection = [
   { name: 'Projects', path: '/' },
   { name: 'CV', path: '/' },
-  { name: 'About', path: '/' },
+  { name: 'About', path: '/about-page' },
 ];
